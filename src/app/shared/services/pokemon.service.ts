f61d17f8aa0075446b14e8d7161c4cc0d6530831
RonaldChicaVera <rchica@rootstack.com>
2022-03-26 17:18:17 -0500

    pokemon service

---
+++ src/app/shared/services/pokemon.service.ts
@@ -0,0 +1,56 @@
+import { Injectable } from '@angular/core';
+import { CrudService } from './crud.service';
+import { Observable } from 'rxjs';
+
+import { Pokemon } from '@shared/interfaces/pokemon';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class PokemonService {
+  constructor(private crudService: CrudService) {}
+
+  /***
+   * Obtener Pokemons
+   */
+  getPokemons(author: number): Observable<Pokemon[]> {
+    return this.crudService.get(`pokemons/?idAuthor=${author}`, {});
+  }
+
+  /***
+   * Obtener Pokemon - Por Identificador
+   * @param idPokemon Identificador Pokemon
+   * @constructor
+   */
+  getPokemonById(idPokemon: number): Observable<Pokemon> {
+    return this.crudService.get(`pokemons/${idPokemon}`);
+  }
+
+  /***
+   * Crear Pokemon
+   * @param data Datos Pokemon
+   * @constructor
+   */
+  createPokemon(data: Pokemon, author: number) {
+    return this.crudService.post(`pokemons/?idAuthor=${author}`, data);
+  }
+
+  /***
+   * Actualizar Pokemon
+   * @param idPokemon Identificador Pokemon
+   * @param data Datos Pokemon
+   * @constructor
+   */
+  updatePokemon(idPokemon: number, data: Pokemon) {
+    return this.crudService.put(`pokemons/${idPokemon}`, data);
+  }
+
+  /***
+   * Eliminar Pokemon
+   * @param idPokemon Identificador Pokemon
+   * @constructor
+   */
+  deletePokemon(idPokemon: number) {
+    return this.crudService.delete(`pokemons/${idPokemon}`);
+  }
+}
--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,12 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
-
+import { IcofontComponent } from '@shared/components/icofont/iconfont.component';
 
 @NgModule({
-  declarations: [],
-  imports: [
-    CommonModule
-  ]
+  declarations: [IcofontComponent],
+  imports: [CommonModule],
+  exports: [IcofontComponent],
 })
-export class SharedModule { }
+export class SharedModule {}
